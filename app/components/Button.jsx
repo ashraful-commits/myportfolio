fe1e54fecae2e22bfce1bf5c03664553ea045713
ashraful-commits <ashrfulalam10000@gmail.com>
2023-07-14 06:12:02 +0600

    button change

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ button, icon }) => {
   return (
     <button className="hover:border-white group bg-teal-500 text-xl pl-5 text-white flex items-center gap-5  mx-auto   mt-5 rounded-md hover:bg-teal-700 transition-colors delay-100 hover:shadow-2xl hover:shadow-white ">
       {button}
-      <span className="darK:bg-white bg-gray-500 group-hover:-rotate-90 dark:text-white h-full inline-block p-5">
+      <span className="darK:bg-white rounded-md bg-gray-500 group-hover:-rotate-90 dark:text-white h-full inline-block p-5">
         {icon}
       </span>
     </button>
