e25aca79d619594a2354ed9f13397eb311b9c6fc
ashraful-commits <ashrfulalam10000@gmail.com>
2023-07-14 06:51:54 +0600

    divider

--- app/Divider.jsx
+++ app/Divider.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Divider = () => {
   return (
     <div className="flex w-full h-[.2px] mb-10 justify-center items-center">
-      <span className=" w-[30%] h-[.2px] bg-teal-200 shadow-2xl shadow-gray-100"></span>
+      <span className=" w-[50%] h-[.2px] bg-teal-200 shadow-2xl shadow-gray-100"></span>
       <span className="w-2 h-2 rounded-full bg-teal-500 mx-5"></span>
-      <span className="w-[30%] h-[.2px] bg-teal-200 shadow-2xl shadow-gray-100"></span>
+      <span className="w-[50%] h-[.2px] bg-teal-200 shadow-2xl shadow-gray-100"></span>
     </div>
   );
 };
